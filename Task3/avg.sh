d3b87e32ce220fb5e050cc908e8601d93b6175d3
gajendra <gajendra.gulgulia@tum.de>
2018-01-03 13:20:01 +0100

    corrected averaging formula divided by 6 for averaging instead of 5 (incorrect)

--- Task3/avg.sh
+++ Task3/avg.sh
@@ -8,7 +8,7 @@ do
 	current_size=$((2**(6+j)))
 	matrix=$current_size'x'$current_size
 	echo -e "FOR MATRICES " $matrix >> avg_results.csv
-	cat result_$matrix.csv | awk -F'Computation time: ' '{sum1+=$2; } END { print "Avg Computation time: = " sum1/5 }' >> avg_results.csv
-	cat result_$matrix.csv | awk -F'MPI time:         ' '{sum2+=$2; } END { print "Avg MPI Time: = " sum2/5 }' >> avg_results.csv
+	cat result_$matrix.csv | awk -F'Computation time: ' '{sum1+=$2; } END { print "Avg Computation time: = " sum1/6 }' >> avg_results.csv
+	cat result_$matrix.csv | awk -F'MPI time:         ' '{sum2+=$2; } END { print "Avg MPI Time: = " sum2/6 }' >> avg_results.csv
 echo -e '\n' >> avg_results.csv
 done
