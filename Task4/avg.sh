1244c363040b4e4461cd5f9507c39fb15d07ea21
gajendra <gajendra.gulgulia@tum.de>
2018-01-07 15:57:53 +0100

    Bash file modified to read results from directories of Phase1 and Phase2 The bash file can browse to directories of Phase1 and Phase2 and compute and store the mean in a separate file in the respective direcory

--- Task4/avg.sh
+++ Task4/avg.sh
@@ -2,12 +2,28 @@
 
 rm avg_results.csv
 
-for j in {0..6}
-do
-	current_size=$((2**(6+j)))
-	matrix=$current_size'x'$current_size
-	echo -e "FOR MATRICES " $matrix >> avg_results.csv
-	cat result_$matrix.csv | awk -F'Computation time: ' '{sum1+=$2; } END { print "Avg Computation time: = " sum1/6 }' >> avg_results.csv
-	cat result_$matrix.csv | awk -F'MPI time:         ' '{sum2+=$2; } END { print "Avg MPI Time: = " sum2/6 }' >> avg_results.csv
-echo -e '\n' >> avg_results.csv
-done
+declare -a array_compute_time=()
+declare -a array_mpi_time=()
+declare -a array_variance=()
+declare -a array_standard_deviation=()
+for i in {1..2}
+do 
+    for j in {0..6}
+    do
+
+	    current_size=$((2**(6+j)))
+	    matrix=$current_size'x'$current_size
+
+        READ_FILE="Phase$i/result_$matrix.csv"
+        WRITE_FILE="Phase$i/avg_results.csv"
+       # echo -e $READ_FILE
+       # echo -e $WRITE_FILE
+
+	    echo -e "FOR MATRICES " $matrix >> $WRITE_FILE 
+	    cat $READ_FILE | awk -F'Computation time: ' '{sum1+=$2; } END { print "Avg Computation time: = " sum1/6 }' >> $WRITE_FILE 
+	   # cat $READ_FILE | awk -F'Computation time: ' '{$array_compute_time[$i++]=$2; } END' 
+
+	    cat $READ_FILE | awk -F'MPI time: ' '{sum2+=$2; } END { print "Avg MPI Time: = " sum2/6 }' >> $WRITE_FILE 
+    echo -e '\n' >> $WRITE_FILE
+    done
+done 
